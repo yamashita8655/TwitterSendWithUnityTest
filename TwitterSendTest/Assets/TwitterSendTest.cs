8e68ae79247e15202115cc840d7ce6cc4e196b5a
machy <machy@fizz.works>
2019-02-02 20:08:30 +0900

    Twitter投稿機能テスト

---
+++ TwitterSendTest/Assets/TwitterSendTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TwitterSendTest : MonoBehaviour {
+
+	[SerializeField]Text DebugOutputText;
+
+	private string OutputText = "";
+
+	public void OnClickTwitterSendButton() {
+		SWorker.SocialWorker.PostTwitter("Test", "", (SWorker.SocialWorkerResult swres) => {
+				string res = "" + swres;
+				AddDebugText(res);
+			}
+		);
+
+	}
+	
+	private void AddDebugText(string text) {
+		OutputText += text + "\n";
+		DebugOutputText.text = OutputText;
+	}
+}
